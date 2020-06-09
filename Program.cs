006b003fee5bd71964cc2411e83ce9b13b8da963
Иванов Евгений Александрович <IvanovEA@72to.ru>
2020-06-09 12:42:14 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sync_FtpToOracle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
